9aa7180b1f4bb518766a3fc521dc67ab7b4e85b9
zibby <zibby@zibby.technology>
2024-06-19 12:33:49 +0100

    Cache cals behind nginx

--- docker.sh
+++ docker.sh
@@ -1,1 +1,7 @@
-docker run --name test-nginx -p 8080:80 -v $(pwd):/usr/share/nginx/html:ro -d nginx
+docker run --name test-nginx \
+	-p 8080:80 \
+	-v "$(pwd):/usr/share/nginx/html:ro" \
+	-v "$(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro" \
+	-v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro" \
+	nginx
+
