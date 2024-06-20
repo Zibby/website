18495684b1bd73e80804280a66fdcbb14fd0c52d
zibby <zibby@zibby.technology>
2024-06-20 02:57:48 +0100

    Revert "Cache cals behind nginx"
    This reverts commit 9aa7180b1f4bb518766a3fc521dc67ab7b4e85b9.

--- docker.sh
+++ docker.sh
@@ -1,7 +1,1 @@
-docker run --name test-nginx \
-	-p 8080:80 \
-	-v "$(pwd):/usr/share/nginx/html:ro" \
-	-v "$(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro" \
-	-v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro" \
-	nginx
-
+docker run --name test-nginx -p 8080:80 -v $(pwd):/usr/share/nginx/html:ro -d nginx
